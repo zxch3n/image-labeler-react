fd5d0d7d706662415aae7a923971fb4540b6700b
DESKTOP-VOMF6EQ\Remch <remch183@outlook.com>
2019-06-16 21:07:10 +0800

    add demo

---
+++ src/Demo.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Annotator} from './Annotator';
+
+ReactDOM.render(
+  <div>
+      <Annotator 
+        height={600} 
+        width={600} 
+        imageUrl={""} 
+        asyncUpload={async (labeledData)=>{
+            // upload labeled data
+        }} 
+        types={['A', 'B', 'Cylinder']}
+        defaultType={"Cylinder"}
+        style={{
+          width: 640,
+          height: 680,
+          margin: "20px auto",
+          position: "relative",
+          backgroundColor: "#368",
+          borderRadius: 8,
+          padding: 10
+        }}/>
+  </div>,
+  document.body
+);
+
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+    context: __dirname,
+    entry: ["@babel/polyfill", "./src/Demo.tsx", "./src/demo.html"],
+    output: {
+        filename: "app.js",
+        path: path.join(__dirname, "/demoDist"),
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[tj]sx?$/,
+                exclude: /node_modules/,
+                loaders: ["babel-loader"],
+            },
+            {
+                test: /\.html$/,
+                loader: "file-loader?name=[name].[ext]",
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {},
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        function () {
+            this.plugin("done", function (stats) {
+                if (stats.compilation.errors && stats.compilation.errors.length) {
+                    console.log(stats.compilation.errors);
+                    process.exit(1);
+                }
+            });
+        },
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        hot: true,
+        contentBase: './demoDist'
+    }
+}
