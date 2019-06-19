9762671f1f04c3a26da5eaf7fe89f1cee8229e4a
DESKTOP-VOMF6EQ\Remch <remch183@outlook.com>
2019-06-19 16:05:51 +0800

    fix sceneType bug

--- src/Annotator.tsx
+++ src/Annotator.tsx
@@ -697,7 +697,7 @@ export class Annotator extends React.Component<Props, State>{
         };
 
         if (this.props.sceneTypes) {
-            data['sceneTypes'] = this.state.sceneType;
+            data['sceneType'] = this.state.sceneType;
         }
 
         return data;
